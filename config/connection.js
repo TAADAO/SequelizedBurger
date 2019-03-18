0ef788e34e29b1abf646a1d0b05674505683a8d7
Robert O <romohund@gmail.com>
2019-03-17 23:25:58 -0500

    EatDaBurger Sequelized

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "trebor",
+        host: "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "zb06p5ig68yun0rp",
+        password: "dfw24h8xvuhv5qb0",
         database: "b09lr7qjdqxfnu42",
         PORT: 8080
     });
